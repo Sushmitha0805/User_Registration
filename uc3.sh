028c63568c0847c5a3f20177f30bc9e3f4500687
Sushmitha0805 <69563297+Sushmitha0805@users.noreply.github.com>
2020-08-21 08:17:30 +0530

    Update uc3.sh

--- uc3.sh
+++ uc3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 shopt -s extglob
 
-pat="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
+pat="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,3})$"
 echo  " enter the email" email
 read email
 
